2222678c6c7ba33349ebe69ab624974bac0aa103
francisco <franciscoau1238@gmail.com>
2025-06-22 22:44:26 -0300

    suibindo o resto

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,48 +8,60 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter com WebView',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
-      ),
-      home: const MyHomePage(title: 'SOS Meio AMbiente'),
+      home: WebViewExample(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class WebViewExample extends StatefulWidget {
+  const WebViewExample({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<WebViewExample> createState() => _WebViewExampleState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  late final WebViewController _controller;
+class _WebViewExampleState extends State<WebViewExample> {
+  InAppWebViewController? webViewController;
 
   @override
   void initState() {
     super.initState();
-    _controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted) // 👉 Habilita JS
-      ..loadRequest(Uri.parse('http://10.0.2.2:8080/principal'));
+    _solicitarPermissoes();
+  }
+
+  Future<void> _solicitarPermissoes() async {
+    await [
+      Permission.location,
+      Permission.camera,
+      Permission.storage,
+    ].request();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        backgroundColor: Colors.white24,
+      appBar: AppBar(title: const Text('SOS Meio Ambiente')),
+      body: InAppWebView(
+        initialUrlRequest: URLRequest(
+          url: WebUri('https://sos-web-production.up.railway.app/denuncia'),
+        ),
+        initialSettings: InAppWebViewSettings(
+          javaScriptEnabled: true,
+          geolocationEnabled: true,
+          mediaPlaybackRequiresUserGesture: false,
+        ),
+        onWebViewCreated: (controller) {
+          webViewController = controller;
+        },
+        androidOnGeolocationPermissionsShowPrompt: (controller, origin) async {
+          return GeolocationPermissionShowPromptResponse(
+              origin: origin, allow: true, retain: true);
+        },
       ),
-      body: WebViewWidget(controller: _controller),
     );
   }
 }
